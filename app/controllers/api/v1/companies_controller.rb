d0bc272eb3eb269cc582f26821bf654ff57261da
Patrick Lindsay <patrick.lindsay@sage.com>
2015-07-23 18:08:47 +0100

    Remove binding and limit filter

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CompaniesController < ApplicationController
   before_filter :restrict_access
-  before_filter :require_query
+  before_filter :require_query, only: [:index]
 
   # GET /
   def index
@@ -18,7 +18,6 @@ class Api::V1::CompaniesController < ApplicationController
     company_number = params[:id]
 
     results = client.company(company_number)
-    require 'pry'; binding.pry
 
     if results.nil?
       render_error_and_return(connection_error)
